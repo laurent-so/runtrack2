7bad09be5436ed48ca4be91c978431d221a0c390
laurent-so <laurent.so@laplateforme.io>
2023-05-10 10:23:16 +0200

    Create index0.php

---
+++ jour01/job00/index0.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Job00</title>
+    </head>
+    <body>
+        <?php echo "Hello LaPlateforme!"; 
+        ?>
+    </body>
+</html>
