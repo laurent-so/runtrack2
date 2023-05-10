1c9e8f1416d956f9c6c9f02e499f87b59eb17240
laurent-so <laurent.so@laplateforme.io>
2023-05-10 10:29:26 +0200

    Create index1.php

---
+++ jour01/job01/index1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Job01x</title>
+        <meta charset="utf-8">
+    </head> 
+
+    <body>
+        <h1>Variables</h1>
+    <?php
+    $str = "LaPlateforme";
+    echo $str;
+    $str2 = "Vive";
+    $str3 = "!";
+    echo $str2 . " " . $str . " " . $str3;
+    ?>
+    <?php
+    $val = 6;
+    echo $val;
+    $val += 4;
+    echo $val;
+    ?>
+    <?php
+    $myBool = true;
+    echo $myBool;
+    $myBool = false;
+    echo $myBool;
+    ?>
+    </body>
+</html>
